617893375ec90f0a2c9fb2448ad16951cfaa7eb0
MasterDalK <mastrdalk@gmail.com>
2021-01-12 15:24:09 -0500

    Remove old touhou data, add teams for initial gup data

---
+++ src/js/data/12012021.js
@@ -0,0 +1,120 @@
+dataSetVersion = "2021-01-12"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Team",
+    key: "team",
+    tooltip: "Check this to restrict to certain teams.",
+    checked: false,
+    sub: [
+       {
+         "name": "Anglerfish Team",
+         "Key": "anglerfish"
+       },
+       {
+         "name": "Turtle Team",
+         "Key": "turtle"
+       },
+       {
+         "name": "Hippo Team",
+         "Key": "hippo"
+       },
+       {
+         "name": "Rabbit Team",
+         "Key": "rabbit"
+       },
+       {
+         "name": "Duck Team",
+         "Key": "duck"
+       },
+       {
+         "name": "Mallard Team",
+         "Key": "mallard"
+       },
+       {
+         "name": "Leopon Team",
+         "Key": "leopon"
+       },
+       {
+         "name": "Anteater Team",
+         "Key": "anteater"
+       },
+       {
+         "name": "Shark Team",
+         "Key": "shark"
+       },
+       {
+         "name": "Ooarai Girls Academy",
+         "Key": "ooarai"
+       },
+       {
+         "name": "St. Gloriana Girls High School",
+         "Key": "gloriana"
+       },
+       {
+         "name": "Saunders Girls High School",
+         "Key": "saunders"
+       },
+       {
+         "name": "Anzio Girls High School",
+         "Key": "anzio"
+       },
+       {
+         "name": "Pravda Girls High School",
+         "Key": "pravda"
+       },
+       {
+         "name": "Kuromorimine Girls High School",
+         "Key": "kuromormine"
+       },
+       {
+         "name": "Chi-Ha-Tan Academy",
+         "Key": "chihatan"
+       },
+       {
+         "name": "Jatkosota High School",
+         "Key": "finland"
+       },
+       {
+         "name": "All-Stars University Team",
+         "Key": "allstars"
+       },
+       {
+         "name": "Bellwall Academy",
+         "Key": "bellwall"
+       },
+       {
+         "name": "Tatenashi High School",
+         "Key": "tatenashi"
+       },
+       {
+         "name": "Count High School",
+         "Key": "count"
+       },
+       {
+         "name": "Maginot Girls' Academy",
+         "Key": "maginot"
+       },
+       {
+         "name": "Bonple High School",
+         "Key": "bonple"
+       },
+       {
+         "name": "Blue Division High School",
+         "Key": "blue"
+       },
+       {
+         "name": "Koala Forest Academy",
+         "Key": "koala"
+       },
+       {
+         "name": "Japan Sensha-Dō Federation",
+         "Key": "senshado"
+       }
+      ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+];
